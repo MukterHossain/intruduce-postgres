d8c2c33d97db4eca4f131631b1cc23444e810280
MukterHossain <mukter3075@gmail.com>
2025-05-21 18:07:33 +0600

    added some sql function

---
+++ index.sql
@@ -0,0 +1,16 @@
+
+
+
+
+SELECT * FROM employees;
+
+EXPLAIN ANALYSE
+SELECT * FROM employees WHERE empployee_name = 'Olivia Clark';
+
+EXPLAIN ANALYSE
+SELECT * from employees WHERE emp_no = '10004';
+
+CREATE INDEX idx_employees_last_name
+on employees (last_name);
+
+SHOW data_directory;
--- trigger.sql
+++ trigger.sql
@@ -12,3 +12,44 @@ A trigger is a database object in PostgreSQL (and other database management syst
 -- ON table_name
 -- [FOR EACH ROW] 
 -- EXECUTE FUNCTION function_name();
+
+
+
+CREATE table my_users (
+    user_name VARCHAR(50),
+    email VARCHAR(100)
+);
+
+INSERT INTO my_users VALUES('Hasan', 'hasan@gmail.com'), ('Naem', 'naem@gmail.com');
+
+CREATE Table deleted_users_audit (
+    deleted_user_name VARCHAR(50),
+    deletedAt TIMESTAMP
+);
+
+-- Trigger Function
+CREATE or REPLACE Function save_deleted_user()
+RETURNS TRIGGER
+LANGUAGE PLPGSQL
+AS
+$$
+BEGIN
+    INSERT INTO deleted_users_audit VALUES(OLD.user_name, now());
+    RAISE NOTICE 'Deleted user audit log created';
+    RETURN OLD;
+END
+$$
+
+
+CREATE or REPLACE Trigger save_deleted_user_trigger
+BEFORE DELETE
+on my_users
+for EACH ROW
+EXECUTE FUNCTION save_deleted_user();
+
+
+
+SELECT * FROM my_users;
+SELECT * FROM deleted_users_audit;
+
+DELETE FROM my_users WHERE user_name = 'Hasan';
